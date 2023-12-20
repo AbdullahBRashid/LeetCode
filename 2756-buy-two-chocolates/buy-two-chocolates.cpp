318d7b57a8b825012d12034216b3077fd932a4eb
Abdullah Bin Rashid <86962307+AbdullahBRashid@users.noreply.github.com>
2023-12-21 00:35:55 +0500

    Time: 31 ms (5.40%) | Memory: 46.9 MB (58.61%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         int smallestVals[2] = {0};
-        for (int i = 0; i < prices.size(); i++) {
-            if (i == 0) {
-                smallestVals[0] = prices[i];
-                smallestVals[1] = prices[i+1];
-            }
-
-            else if (prices[i] <= smallestVals[0]) {
+        smallestVals[0] = prices[0];
+        smallestVals[1] = prices[1];
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] <= smallestVals[0]) {
                 smallestVals[1] = smallestVals[0];
                 smallestVals[0] = prices[i];
             } else if (prices[i] <= smallestVals[1]) {
