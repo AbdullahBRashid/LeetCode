d66ff56ab90dc3d589058cfbfee12a394ae86471
Abdullah Bin Rashid <86962307+AbdullahBRashid@users.noreply.github.com>
2024-01-27 15:46:31 +0500

    Time: 2 ms (45.46%) | Memory: 10.4 MB (7.18%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> newDigits;
-
-        newDigits = digits;
-        
+    
         for (int i = 0; i < digits.size(); i++) {
             int index = digits.size()-1-i;
-            newDigits[index]++;
-            if (newDigits[index] == 10) {
+            digits[index]++;
+            if (digits[index] == 10) {
                 if (index == 0) {
-                    newDigits[index] = 1;
-                    newDigits.push_back(0);
+                    digits[index] = 1;
+                    digits.push_back(0);
+                    break;
                 } else
-                newDigits[index] = 0;
+                digits[index] = 0;
             } else {
                 break;
             }
         }
 
-        return newDigits;
+        return digits;
     }
 };
