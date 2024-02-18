47a6dcdb978f69e7d534c904dfdebc80aca580c5
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2024-02-18 10:18:58 +0500

    Time: 4 ms (67.16%) | Memory: 22 MB (96.85%) - LeetSync

---
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int returnToBoundaryCount(vector<int>& nums) {
+        int cur_pos = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            cur_pos += nums[i];
+
+            if (nums[i] == 0) continue;
+
+            if (cur_pos == 0) count++;
+        }
+
+        return count;
+    }
+};
