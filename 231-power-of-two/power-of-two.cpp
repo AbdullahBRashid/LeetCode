0c0bd6669b87d848ea94c486915c85cf71e45ea0
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2024-02-19 14:50:42 +0500

    Time: 0 ms (100.00%) | Memory: 7.2 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-        int count = 0;
-        while ( n != 0) {
-            int bit = n & 1;
-            if (bit == 1) count++;
-            n >>= 1;
-        }
-        if (count == 0 || count == 1) return true;
-        return false;
+        return n > 0 && not ( n & n-1);
     }
 };
