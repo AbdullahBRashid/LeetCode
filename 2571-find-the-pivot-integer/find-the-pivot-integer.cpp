71babb035347f58c15cee61d874691aef086730c
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2024-03-13 22:00:37 +0500

    Time: 4 ms (35.26%) | Memory: 7.2 MB (73.82%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int lhs = 0;
+        int rhs = 0;
+
+        int pivot;
+
+        for (int i = 0; i <= n; i++) {
+            rhs += i;
+        }
+
+        cout << rhs << endl;
+
+        for (int i = 1; i <= n; i++) {
+            lhs += i;
+            rhs -= i-1;
+
+            if (lhs == rhs) return i;
+        }
+
+        return -1;
+
+
+    }
+};
