60a436f7cb4b2a6303624a1bb8f4d8de7549e384
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2024-03-22 11:54:08 +0500

    Time: 186 ms (29.47%) | Memory: 130.8 MB (18.21%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -18,7 +18,7 @@ public:
             node = node->next;
         }
 
-        for (int i = 0; i < arr.size(); i++) {
+        for (int i = 0; i < arr.size() / 2; i++) {
             if (arr[i] != arr[arr.size() - i - 1]) return false;
         }
 
